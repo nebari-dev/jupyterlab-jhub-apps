88911c27d98d4c7c67790cfa57942f15923a61fb
Nick Byrne <byrnen8@tcd.ie>
2024-10-02 18:21:54 -0300

    experiment with widget extension

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,7 @@ import {
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 import { deployAppIcon } from './icons';
-import { DocumentWidget } from '@jupyterlab/docregistry';
-
+import { Widget } from '@lumino/widgets';
 namespace CommandIDs {
   /**
    * Opens the URL to deploy the application with pre-populated fields
@@ -17,14 +16,55 @@ interface IDeployAppArgs {
    * The origin of the command e.g. main-menu, context-menu, etc.
    */
   origin?: string;
+  /**
+   * Whether or not the deployment form should be opened in headless mode
+   */
+  headless?: boolean;
 }
+
+interface IPathWidget {
+  /**
+   * The context object associated with the widget.
+   * Contains the metadata and information about the widget.
+   */
+  context: {
+    /**
+     * The path of the current document or resource associated with the widget.
+     * Typically represents the file path or location of the notebook or document.
+     */
+    path: string;
+  };
+}
+
+const hasContextPath = (widget: any): widget is IPathWidget => {
+  return widget && widget.context && typeof widget.context.path === 'string';
+};
 
 const jhubAppsPlugin: JupyterFrontEndPlugin<void> = {
   id: 'jupyterlab-jhub-apps:commands',
   description: 'Adds additional commands used by jhub-apps.',
   autoStart: true,
   activate: (app: JupyterFrontEnd) => {
-    const openURL = (url: string) => {
+    const deploymentFormUrl = '/services/japps/create-app';
+
+    const openInIFrame = (deployUrl: string) => {
+      try {
+        const iframe = document.createElement('iframe');
+        iframe.src = deployUrl;
+        iframe.width = '100%';
+        iframe.height = '100%';
+        iframe.style.border = 'none';
+
+        const widget = new deployAppWidget();
+        widget.node.appendChild(iframe);
+
+        app.shell.add(widget, 'main');
+      } catch (error) {
+        console.warn(`Error opening ${deployUrl} in iframe: ${error}`);
+      }
+    };
+
+    const openInWindow = (url: string) => {
       try {
         window.open(url, '_blank', 'noopener,noreferrer');
       } catch (error) {
@@ -37,6 +77,7 @@ const jhubAppsPlugin: JupyterFrontEndPlugin<void> = {
         case 'main-menu':
           return undefined;
         case 'context-menu':
+        case 'toolbar':
         case undefined:
         default:
           return deployAppIcon;
@@ -44,19 +85,29 @@ const jhubAppsPlugin: JupyterFrontEndPlugin<void> = {
     };
 
     app.commands.addCommand(CommandIDs.deployApp, {
-      execute: () => {
+      execute: (args: IDeployAppArgs) => {
         const currentWidget = app.shell.currentWidget;
-        const currentNotebookPath =
-          currentWidget && currentWidget instanceof DocumentWidget
-            ? currentWidget.context.path
-            : '';
-        let deployUrl;
-        if (currentNotebookPath !== '') {
-          deployUrl = `/services/japps/create-app?filepath=${encodeURIComponent(currentNotebookPath)}`;
+        const currentPath = hasContextPath(currentWidget)
+          ? currentWidget.context.path
+          : '';
+
+        let queryParameters: string;
+        if (currentPath !== '') {
+          queryParameters = `?filepath=${encodeURIComponent(currentPath)}`;
         } else {
-          deployUrl = '/services/japps/create-app';
+          queryParameters = '?';
         }
-        openURL(deployUrl);
+
+        if (args.headless) {
+          if (queryParameters !== '?') {
+            queryParameters += '&headless=true';
+          } else {
+            queryParameters += 'headless=true';
+          }
+          openInIFrame(`${deploymentFormUrl}${queryParameters}`);
+        } else {
+          openInWindow(`${deploymentFormUrl}${queryParameters}`);
+        }
       },
       label: 'Deploy App',
       icon: calculateIcon
@@ -67,3 +118,13 @@ const jhubAppsPlugin: JupyterFrontEndPlugin<void> = {
 const plugins = [jhubAppsPlugin];
 
 export default plugins;
+
+class deployAppWidget extends Widget {
+  constructor() {
+    super();
+    this.addClass('jp-example-view');
+    this.id = 'deploy-app';
+    this.title.label = 'Deploy App';
+    this.title.closable = true;
+  }
+}
