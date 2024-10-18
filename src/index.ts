3c32f3999a0acae0fca79083327fcdce60f62ee9
Nick Byrne <byrnen8@tcd.ie>
2024-10-18 11:15:00 -0300

    Add restorer

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,34 @@
 import {
   JupyterFrontEnd,
-  JupyterFrontEndPlugin
+  JupyterFrontEndPlugin,
+  ILayoutRestorer
 } from '@jupyterlab/application';
 import { deployAppIcon } from './icons';
+import { WidgetTracker, IFrame, MainAreaWidget } from '@jupyterlab/apputils';
 import { Widget } from '@lumino/widgets';
+
+const DEFAULT_BASE_URL = '/services/japps/create-app';
 namespace CommandIDs {
   /**
-   * Opens the URL to deploy the application with pre-populated fields
+   * Opens the URL for deploying an app with pre-populated fields.
    */
   export const deployApp = 'jhub-apps:deploy-app';
 }
 
 interface IDeployAppArgs {
   /**
-   * The origin of the command e.g. main-menu, context-menu, etc.
+   * The origin of the command e.g. main-menu, context-menu, etc.,
+   * which determines the icon and label to display.
    */
   origin?: string;
   /**
-   * Whether or not the deployment form should be opened in headless mode
+   * The base URL of the app deployment form.
    */
-  headless?: boolean;
+  baseUrl?: string;
+  /**
+   * Additional query parameters to be appended to the baseUrl.
+   */
+  queryParameters?: Record<string, string>;
 }
 
 interface IPathWidget {
@@ -40,38 +49,21 @@ const hasContextPath = (widget: any): widget is IPathWidget => {
   return widget && widget.context && typeof widget.context.path === 'string';
 };
 
+export function buildQueryString(params: Record<string, string>): string {
+  return Object.entries(params)
+    .map(
+      ([key, value]) =>
+        `${encodeURIComponent(key)}=${encodeURIComponent(value)}`
+    )
+    .join('&');
+}
+
 const jhubAppsPlugin: JupyterFrontEndPlugin<void> = {
   id: 'jupyterlab-jhub-apps:commands',
   description: 'Adds additional commands used by jhub-apps.',
   autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
-    const deploymentFormUrl = '/services/japps/create-app';
-
-    const openInIFrame = (deployUrl: string) => {
-      try {
-        const iframe = document.createElement('iframe');
-        iframe.src = deployUrl;
-        iframe.width = '100%';
-        iframe.height = '100%';
-        iframe.style.border = 'none';
-
-        const widget = new deployAppWidget();
-        widget.node.appendChild(iframe);
-
-        app.shell.add(widget, 'main');
-      } catch (error) {
-        console.warn(`Error opening ${deployUrl} in iframe: ${error}`);
-      }
-    };
-
-    const openInWindow = (url: string) => {
-      try {
-        window.open(url, '_blank', 'noopener,noreferrer');
-      } catch (error) {
-        console.warn(`Error opening ${url}: ${error}`);
-      }
-    };
-
+  optional: [ILayoutRestorer],
+  activate: (app: JupyterFrontEnd, restorer: ILayoutRestorer | null) => {
     const calculateIcon = (args: IDeployAppArgs) => {
       switch (args.origin) {
         case 'main-menu':
@@ -85,33 +77,99 @@ const jhubAppsPlugin: JupyterFrontEndPlugin<void> = {
     };
 
     app.commands.addCommand(CommandIDs.deployApp, {
-      execute: (args: IDeployAppArgs) => {
+      execute: ({
+        origin,
+        baseUrl = DEFAULT_BASE_URL,
+        queryParameters = {}
+      }: IDeployAppArgs) => {
         const currentWidget = app.shell.currentWidget;
-        const currentPath = hasContextPath(currentWidget)
+        const filepath = hasContextPath(currentWidget)
           ? currentWidget.context.path
           : '';
 
-        let queryParameters: string;
-        if (currentPath !== '') {
-          queryParameters = `?filepath=${encodeURIComponent(currentPath)}`;
-        } else {
-          queryParameters = '?';
-        }
+        const selectedFilepath =
+          queryParameters.filepath !== undefined
+            ? queryParameters.filepath
+            : filepath;
 
-        if (args.headless) {
-          if (queryParameters !== '?') {
-            queryParameters += '&headless=true';
-          } else {
-            queryParameters += 'headless=true';
+        const headless = (() => {
+          const value = queryParameters.headless;
+          if (value === undefined) {
+            return 'true';
           }
-          openInIFrame(`${deploymentFormUrl}${queryParameters}`);
+          if (
+            typeof value === 'string' &&
+            ['true', 'false'].includes(value.toLowerCase())
+          ) {
+            return value.toLowerCase();
+          }
+          console.warn(`
+            Invalid 'headless' value: ${value}. Defaulting to 'true'.
+            `);
+          return 'true';
+        })();
+
+        const updatedQueryParameters = {
+          ...queryParameters,
+          headless: headless,
+          filepath: selectedFilepath
+        };
+
+        let mainAreaWidget: MainAreaWidget<DeployAppWidget> | undefined;
+        if (headless === 'true') {
+          try {
+            if (!mainAreaWidget || mainAreaWidget.isDisposed) {
+              const content = new DeployAppWidget({
+                origin: origin,
+                baseUrl: baseUrl,
+                queryParameters: updatedQueryParameters
+              });
+              mainAreaWidget = new MainAreaWidget({ content });
+            }
+
+            if (!tracker.has(mainAreaWidget)) {
+              tracker.add(mainAreaWidget);
+            }
+
+            // non-empty origin implies user has called command e.g. main-menu click.
+            // guards against (what appears to be) command execution from tracker
+            // on session restore.
+            if (origin) {
+              if (!mainAreaWidget.isAttached) {
+                app.shell.add(mainAreaWidget, 'main');
+              }
+            }
+          } catch (error) {
+            console.warn(`Error opening in headless mode: ${error}`);
+          }
         } else {
-          openInWindow(`${deploymentFormUrl}${queryParameters}`);
+          try {
+            // no restore logic required here as opening in a separate window
+            const completeUrl = `${baseUrl}?${buildQueryString(updatedQueryParameters)}`;
+            window.open(completeUrl, '_blank', 'noopener,noreferrer');
+          } catch (error) {
+            console.warn(`Error opening in window: ${error}`);
+          }
         }
       },
       label: 'Deploy App',
       icon: calculateIcon
     });
+
+    const tracker = new WidgetTracker<MainAreaWidget<DeployAppWidget>>({
+      namespace: 'jhub-apps'
+    });
+
+    if (restorer) {
+      restorer.restore(tracker, {
+        command: CommandIDs.deployApp,
+        args: widget => ({
+          baseUrl: widget.content.getBaseUrl(),
+          queryParameters: widget.content.getQueryParameters()
+        }),
+        name: widget => widget.content.getCompleteUrl()
+      });
+    }
   }
 };
 
@@ -119,12 +177,39 @@ const plugins = [jhubAppsPlugin];
 
 export default plugins;
 
-class deployAppWidget extends Widget {
-  constructor() {
+class DeployAppWidget extends Widget {
+  private _iframe: IFrame;
+  private _baseUrl: string;
+  private _queryParameters: Record<string, string>;
+
+  constructor({
+    queryParameters = {},
+    baseUrl = DEFAULT_BASE_URL
+  }: IDeployAppArgs) {
     super();
-    this.addClass('jp-example-view');
-    this.id = 'deploy-app';
+    this._queryParameters = queryParameters;
+    this._baseUrl = baseUrl;
+
+    this.addClass('jp-deploy-app-widget');
+    this.id = 'deploy-app-jupyterlab';
     this.title.label = 'Deploy App';
     this.title.closable = true;
+
+    this._iframe = new IFrame();
+    this._iframe.sandbox = ['allow-scripts', 'allow-same-origin'];
+    this._iframe.url = this.getCompleteUrl();
+    this.node.appendChild(this._iframe.node);
+  }
+
+  getQueryParameters(): Record<string, string> {
+    return this._queryParameters;
+  }
+
+  getBaseUrl(): string {
+    return this._baseUrl;
+  }
+
+  getCompleteUrl(): string {
+    return `${this._baseUrl}?${buildQueryString(this._queryParameters)}`;
   }
 }
